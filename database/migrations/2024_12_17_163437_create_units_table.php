3a6188ab320317d590a985020c858f2a52c5b278
usulix <getnetez@gmail.com>
2024-12-18 10:21:43 -0700

    more new

---
+++ database/migrations/2024_12_17_163437_create_units_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('number')->nullable();
+            $table->string('make')->nullable();
+            $table->string('model')->nullable();
+            $table->unsignedInteger('mileage')->nullable();
+            $table->foreignId('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('units');
+    }
+};
